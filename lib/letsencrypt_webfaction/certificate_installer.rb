2aff63407fb306f7e7affe5812932e34c18fa9bd
William Johnston <william@johnstonhaus.us>
2016-11-05 14:36:07 -0700

    Clean up rubocop issues

--- lib/letsencrypt_webfaction/certificate_installer.rb
+++ lib/letsencrypt_webfaction/certificate_installer.rb
@@ -11,21 +11,28 @@ module LetsencryptWebfaction
     end
 
     def install!
-      # Save the certificate and key
-
-      server = XMLRPC::Client.new2(@credentials.api_server)
-      login_resp = server.call('login', @credentials.username, @credentials.password, @credentials.servername, WEBFACTION_API_VERSION)
-      session_id = login_resp[0]
-
-      cert_list = server.call('list_certificates', session_id)
+      cert_list = server_client.call('list_certificates', session_id)
       action = if cert_list.find { |cert| cert['name'] == @cert_name }
                  'update_certificate'
                else
                  'create_certificate'
-      end
-      server.call(action, session_id, @cert_name, @certificate.to_pem, @certificate.request.private_key.to_pem, @certificate.chain_to_pem)
+               end
+      server_client.call(action, session_id, @cert_name, @certificate.to_pem, @certificate.request.private_key.to_pem, @certificate.chain_to_pem)
 
       true
+    end
+
+    private
+
+    def server_client
+      @server_client ||= XMLRPC::Client.new2(@credentials.api_server)
+    end
+
+    def session_id
+      @session_id ||= begin
+        login_resp = server_client.call('login', @credentials.username, @credentials.password, @credentials.servername, WEBFACTION_API_VERSION)
+        login_resp[0]
+      end
     end
   end
 end
